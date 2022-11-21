de6ae07c9b3d0727f0f208655da48c5f52081107
lyubov <lyubov.chikonina@mail.ru>
2022-11-21 08:03:38 +0300

    программа 41 задачи

---
+++ task_41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 4
+Console.Clear();
+System.Console.WriteLine("Введите числа через пробел");
+
+int[] resultay = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+int count(int[] mas)
+{
+    int i = 0;
+    foreach (var item in mas)
+    {
+        if (item > 0) i++;
+    }
+    return i;
+}
+System.Console.WriteLine($"Введенные числа : {String.Join("|", resultay)} элеметов больше нуля- {count(resultay)}");
